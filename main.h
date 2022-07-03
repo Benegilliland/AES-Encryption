f640f4eaad706cc1f649228393e71a32ae63befd
Benjamin Gilliland <73527724+Benegilliland@users.noreply.github.com>
2022-07-03 13:18:51 +0100

    Delete main.h

--- main.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "transformations.h"
-
-void InitSbox(byte* sbox);
-void InitInvSbox(byte* sbox, byte* inv_sbox);
-void KeyExpansion(byte* key, byte* w, byte* sbox);
-void AESCipher(byte* input, byte* output, byte* w, byte* sbox);
-void InvAESCipher(byte* input, byte* output, byte* w, byte* sbox);
